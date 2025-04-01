68be296494e4d3769740626ec625ef7a100f2e39
Priyanka Pradhan <priyankapradhan212@gmail.com>
2025-04-02 12:20:28 +1300

    Updated configurations

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// src/setupTests.js
+import { MemoryRouter } from 'react-router-dom';
+
+jest.mock('react-router-dom', () => ({
+  BrowserRouter: MemoryRouter,
+  Routes: jest.fn(),
+  Route: jest.fn(),
+}));
+
