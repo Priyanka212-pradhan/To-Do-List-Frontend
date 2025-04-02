4c72e1c5d5e1f406f690b2e05505568ff688334f
Priyanka Pradhan <priyankapradhan212@gmail.com>
2025-04-02 22:16:49 +1300

    Unit testing and integration for navigation

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,14 +1,9 @@
+// src/setupTests.js
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-// src/setupTests.js
-import { MemoryRouter } from 'react-router-dom';
 
-jest.mock('react-router-dom', () => ({
-  BrowserRouter: MemoryRouter,
-  Routes: jest.fn(),
-  Route: jest.fn(),
-}));
-
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
